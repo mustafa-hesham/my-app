f730fbc55c0c6a89310e2d39211c505b42b3f02c
Mustafa Hesham <mustafa.hesham@scandiweb.com>
2022-06-26 00:15:44 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Button  from 'react-bootstrap/Button';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function Hello () {
-  return (
-  <h1>Hello world</h1>
-  );
+function Hello() {
+  return <h1>Hello world</h1>;
 }
-const Bye = <h2>Bye</h2>;
 
+function HelloButton() {
+  return <Button>Hello</Button>;
+}
 
 function App() {
-  
   return (
     <div className="App">
       <header className="App-header">
@@ -21,7 +20,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Hello />
-        <Button>Hello</Button>
+        <HelloButton />
         <a
           className="App-link"
           href="https://reactjs.org"
